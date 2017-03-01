090dece22cfee184690970463bab7a438ad6369e
James Kemp <james@madebyplatform.com>
2017-03-01 22:34:54 +0000

    [rename] Rename boilerplate

--- quotelist/quotelist.php
+++ quotelist/quotelist.php
@@ -10,18 +10,18 @@
  *
  * @link              http://example.com
  * @since             1.0.0
- * @package           Plugin_Name
+ * @package           Quotelist
  *
  * @wordpress-plugin
- * Plugin Name:       WordPress Plugin Boilerplate
- * Plugin URI:        http://example.com/plugin-name-uri/
- * Description:       This is a short description of what the plugin does. It's displayed in the WordPress admin area.
+ * Plugin Name:       Quotelist
+ * Plugin URI:        http://example.com/quotelist-uri/
+ * Description:       Product catalog and request a quote functionality for WordPress.
  * Version:           1.0.0
- * Author:            Your Name or Your Company
- * Author URI:        http://example.com/
+ * Author:            James Kemp
+ * Author URI:        http://jckemp.com/
  * License:           GPL-2.0+
  * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
- * Text Domain:       plugin-name
+ * Text Domain:       quotelist
  * Domain Path:       /languages
  */
 
@@ -32,30 +32,30 @@ if ( ! defined( 'WPINC' ) ) {
 
 /**
  * The code that runs during plugin activation.
- * This action is documented in includes/class-plugin-name-activator.php
+ * This action is documented in includes/class-quotelist-activator.php
  */
-function activate_plugin_name() {
-	require_once plugin_dir_path( __FILE__ ) . 'includes/class-plugin-name-activator.php';
-	Plugin_Name_Activator::activate();
+function activate_quotelist() {
+	require_once plugin_dir_path( __FILE__ ) . 'includes/class-quotelist-activator.php';
+	Quotelist_Activator::activate();
 }
 
 /**
  * The code that runs during plugin deactivation.
- * This action is documented in includes/class-plugin-name-deactivator.php
+ * This action is documented in includes/class-quotelist-deactivator.php
  */
-function deactivate_plugin_name() {
-	require_once plugin_dir_path( __FILE__ ) . 'includes/class-plugin-name-deactivator.php';
-	Plugin_Name_Deactivator::deactivate();
+function deactivate_quotelist() {
+	require_once plugin_dir_path( __FILE__ ) . 'includes/class-quotelist-deactivator.php';
+	Quotelist_Deactivator::deactivate();
 }
 
-register_activation_hook( __FILE__, 'activate_plugin_name' );
-register_deactivation_hook( __FILE__, 'deactivate_plugin_name' );
+register_activation_hook( __FILE__, 'activate_quotelist' );
+register_deactivation_hook( __FILE__, 'deactivate_quotelist' );
 
 /**
  * The core plugin class that is used to define internationalization,
  * admin-specific hooks, and public-facing site hooks.
  */
-require plugin_dir_path( __FILE__ ) . 'includes/class-plugin-name.php';
+require plugin_dir_path( __FILE__ ) . 'includes/class-quotelist.php';
 
 /**
  * Begins execution of the plugin.
@@ -66,10 +66,11 @@ require plugin_dir_path( __FILE__ ) . 'includes/class-plugin-name.php';
  *
  * @since    1.0.0
  */
-function run_plugin_name() {
+function run_quotelist() {
 
-	$plugin = new Plugin_Name();
+	$plugin = new Quotelist();
 	$plugin->run();
 
 }
-run_plugin_name();
+
+run_quotelist();
